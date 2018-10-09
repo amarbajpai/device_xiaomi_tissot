7cbff117d53ca0606a33f9159edc98acc13b30a6
saksham033 <sakshamgoyal02@gmail.com>
2018-10-09 22:43:02 +0200

    Revert "add A/B OTA flags"
    This reverts commit feb4483d0048e98d15edc4d2cac0d5c9ab538934.

--- arrow_tissot.mk
+++ arrow_tissot.mk
@@ -24,25 +24,6 @@ $(call inherit-product, device/xiaomi/tissot/device.mk)
 # Inherit some common Arrow stuff.
 $(call inherit-product, vendor/arrow/config/common.mk)
 
-# A/B updater
-AB_OTA_UPDATER := true
-
-AB_OTA_PARTITIONS += \
-    boot \
-    system
-
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    update_engine \
-    update_engine_sideload \
-    update_verifier
-
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tissot
 PRODUCT_NAME := arrow_tissot
