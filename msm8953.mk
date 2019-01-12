056912c96c0e2224ab050ab5969bb1946937de00
amarbajpai <bajpai.amar3@gmail.com>
2019-01-12 16:12:40 +0530

    tissot: add basic usb hals that report no status change

--- msm8953.mk
+++ msm8953.mk
@@ -273,6 +273,9 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+# Perf
+-include vendor/qcom/common/qti-vendor.mk
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0-impl \
@@ -343,7 +346,7 @@ PRODUCT_COPY_FILES += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.xiaomi_8996
 
 # VNDK
 PRODUCT_PACKAGES += \
