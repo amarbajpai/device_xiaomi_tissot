24ea987d6532e715fb5384ebfb79b637bd5ed260
Amar Bajpai <bajpai.amar3@gmail.com>
2019-06-24 22:30:33 +0200

    PixysOS Bringup

--- device.mk
+++ device.mk
@@ -19,7 +19,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 # Inherit from msm8953-common
 $(call inherit-product, device/xiaomi/msm8953-common/msm8953.mk)
@@ -97,3 +96,7 @@ PRODUCT_PACKAGES_DEBUG += \
 # Verity
 PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/soc/7824900.sdhci/by-name/system
 $(call inherit-product, build/target/product/verity.mk)
+
+# Pixel 2 Camera
+PRODUCT_PACKAGES += \
+    GCam
